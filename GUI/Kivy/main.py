52b47c32b86f54c2f6180a914969846f1e5f2c62
Ajwinarski <ajwinarski@gmail.com>
2019-09-01 16:03:20 -0400

    Finally got settings to modify the UI

--- GUI/Kivy/main.py
+++ GUI/Kivy/main.py
@@ -6,7 +6,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.settings import Settings
 from kivy.uix.settings import SettingsWithSidebar
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, SlideTransition
 
 from kivy.animation import Animation
 from kivy.lang import Builder
@@ -25,13 +25,35 @@ Window.size = (800, 480)
 Window.clearcolor = (0.09411,0.270588,0.231372,1) #rgba(24,69,59,1) / 255
 Window.borderless = True
 
-config = ConfigParser()
 
+class LongpressButton(Factory.Button):
+    __events__ = ('on_long_press', )
 
-# Screen Definition
+    long_press_time = Factory.NumericProperty(1)
+
+    def on_state(self, instance, value):
+        if value == 'down':
+            lpt = self.long_press_time
+            self._clockev = Clock.schedule_once(self._do_long_press, lpt)
+        else:
+            self._clockev.cancel()
+
+    def _do_long_press(self, dt):
+        self.dispatch('on_long_press')
+
+    def on_long_press(self, *largs):
+        pass
+
+""" Screen Definition """
 class MainScreen(Screen):
+
+    # partyMode = BooleanProperty(False)
+
     # def hideConfig(self):
-    #     self.root.ids.configBtn.disabled = True
+    #     if config is self.config:
+    #         for item in config.sections():
+    #             print(config.options(item))
+    #         return False
     pass
 
 class DrinksScreen(Screen):
@@ -40,34 +62,54 @@ class DrinksScreen(Screen):
 class ConfigScreen(Screen):
     pass
 
-class SettingsScreen(Screen):
-    # s = Settings()
-    pass
+# class SettingsScreen(Screen):
+#     # s = Settings()
+#     pass
 
 # Screen Manager Definition
-class ScreenManager(ScreenManager):
-    pass
-    # def __init__(self, **kwargs):
-    #     super(ScreenManager, self).__init__(**kwargs)
-    #     presentation = Builder.load_file("spartender.kv")
-    #     return presentation
+# class ScreenManager(ScreenManager):
+#     def __init__(self, **kwargs):
+#         return super(ScreenManager, self).__init__(**kwargs)
 
 # Load the main menu .kv file
 # presentation = Builder.load_file("spartender.kv")
 
-# Create the screen manager
-# sm = ScreenManager()
-# sm.add_widget(MainScreen(name='main'))
-# sm.add_widget(AnotherScreen(name='other'))
+# Define the config var for settings
+config = ConfigParser()
 
 class SpartenderApp(App):
+
+    screen_names = ListProperty([])
+    sm = ScreenManager(transition = SlideTransition(duration = 0.55))
+
     def build(self):
         self.settings_cls = SettingsWithSidebar
         self.use_kivy_settings = False
-        setting = self.config.get('settings', 'partyMode')
+
+        mainPage = MainScreen(name = "main")
+        drinksPage = DrinksScreen(name='drinks')
+        configPage = ConfigScreen(name='config')
+
+        # Create the screen manager
+        # self.sm = ScreenManager(transition = SlideTransition(duration = 0.8))
+        self.sm.add_widget(mainPage)
+        self.sm.add_widget(drinksPage)
+        self.sm.add_widget(configPage)
+
+        self.sm.current = "main"
+
+        # self.screens = {}
+        # self.available_screens = sorted([
+        #     'Homepage', 'Drinks'
+        # ])
+        # self.screen_names = self.available_screens
+        # curdir = dirname(__file__)
+        # self.available_screens = [join(curdir, 'screens',
+        #     '{}.kv'.format(fn).lower()) for fn in self.available_screens]
+
+        # setting = self.config.get('settings', 'partyMode')
         # return presentation
-        # return ScreenManager()
-        pass
+        return self.sm
 
     def build_config(self, config):
         config.setdefaults('settings', {
@@ -90,7 +132,21 @@ class SpartenderApp(App):
         if config is self.config:
             token = (section, key)
             if token == ('settings', 'partyMode'):
-                print('Party mode set to ', value)
+                # print('Party mode set to ', value)
+                # print(self.sm.screens)
+                if value == '1':
+                    print("Value is true")
+                    self.sm.get_screen('main').ids.configBtn.disabled = True
+                    self.sm.get_screen('main').ids.configBtn.size_hint = (0, 0)
+                elif value == '0':
+                    print("Value is false")
+                    self.sm.get_screen('main').ids.configBtn.disabled = False
+                    self.sm.get_screen('main').ids.configBtn.size_hint = (.25, .4)
+
+                # print(config.get('settings', 'partyMode'))
+
+                # Call the function to hide config button here
+
                 # self.root.ids.configScreen.ids.configBtn.text = "Test"
                 # self.root.ids.configBtn.text = "Worked"
             elif token == ('settings', 'pinMode'):
